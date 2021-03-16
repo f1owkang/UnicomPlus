4e50e75e64a3d0e8eba10a1bbf1bce2ea3a5cbc2
拾贰 <LxDebug@users.noreply.github.com>
2021-03-16 21:20:01 +0800

    Fix scheduler

--- scheduler.py
+++ scheduler.py
@@ -7,7 +7,6 @@ import time
 
 def listdir(path, list_name): 
   for file in os.listdir(path): 
-    file_path = os.path.join(path, file) 
     if os.path.isdir(file_path): 
       listdir(file_path, list_name) 
     elif os.path.splitext(file_path)[1]=='.py': 

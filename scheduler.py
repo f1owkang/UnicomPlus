6b72c73862884cc928e09a5acadc9e1335121c55
拾贰 <LxDebug@users.noreply.github.com>
2021-03-17 01:48:29 +0800

    Fix scheduler

--- scheduler.py
+++ scheduler.py
@@ -37,7 +37,7 @@ def runscheduler(client,username,num):
             logging.info('【任务分配】: ' + task)
             i = importlib.import_module('TaskList.'+task)
             i.main(client,username,num)
-            resetJson('.','.',int(taskNum) + 1)
+            resetJson('./','./',int(taskNum) + 1)
             forNum = forNum + 1
             dtime =random.randint(3,30)
             logging.info('【任务调度】: 延时进行' + str(dtime)+'秒')
@@ -55,7 +55,7 @@ def resetJson(file_old,file_new,num):
         if os.path.splitext(rest)[1]=='.json':
             L.append(os.path.join(file_old,rest)) #创建文件路径
     for f11 in L:
-        with open(f11,'rw') as f:
+        with open(f11,'r') as f:
             data = json.load(f)
             data[0]['taskNum'] = str(num)
             newpath = os.path.join(file_new,os.path.split(f11)[1])
